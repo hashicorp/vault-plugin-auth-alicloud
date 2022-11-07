83c30bb58c1c75af8981dd19675d575c5c1de4fb
JM Faircloth <jmfaircloth@hashicorp.com>
2022-11-07 12:12:55 -0600

    add comments about API endpoints

--- path_login.go
+++ path_login.go
@@ -286,6 +286,13 @@ func (b *backend) getCallerIdentity(header http.Header, rawURL string) (*sts.Get
 	return result, nil
 }
 
+// getSTSEndpoint will build endpoints from the given region. Using the sts
+// GetEndpointRules API method. See the endpoint docs at:
+// https://www.alibabacloud.com/help/en/resource-access-management/latest/api-doc-sts-2015-04-01-endpoint
+//
+// Alicloud Support said that there is not an API to fetch all sts endpoints.
+// See the ticket at:
+// https://workorder-intl.console.aliyun.com/console.htm?msctype=email&mscareaid=sg&mscsiteid=intl&mscmsgid=1690222110700066955&spm=a2c4k.11991735.enc.2&lang=en&accounttraceid=887ed555fd4c43cca57281472422b2ddierr#/ticket/detail/?ticketId=G21GFGGG
 func getSTSEndpoint(regionID string) (string, error) {
 	config := sdk.NewConfig()
 	config.Scheme = "https"
