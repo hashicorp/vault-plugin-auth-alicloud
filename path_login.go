3501daf4a6ab6206103a46a5b49a9454481b01e2
JM Faircloth <jmfaircloth@hashicorp.com>
2022-11-11 13:08:32 -0600

    link github ticket

--- path_login.go
+++ path_login.go
@@ -389,8 +389,7 @@ func (b *backend) getCallerIdentity(header http.Header, rawURL string) (*sts.Get
 // https://www.alibabacloud.com/help/en/resource-access-management/latest/api-doc-sts-2015-04-01-endpoint
 //
 // Alicloud Support said that there is not an API to fetch all sts endpoints.
-// See the ticket at:
-// https://workorder-intl.console.aliyun.com/console.htm?msctype=email&mscareaid=sg&mscsiteid=intl&mscmsgid=1690222110700066955&spm=a2c4k.11991735.enc.2&lang=en&accounttraceid=887ed555fd4c43cca57281472422b2ddierr#/ticket/detail/?ticketId=G21GFGGG
+// See the githbu ticket at: https://github.com/aliyun/alibaba-cloud-sdk-go/issues/577
 func getSTSEndpoint(regionID string) (string, error) {
 	config := sdk.NewConfig()
 	config.Scheme = "https"
